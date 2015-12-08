76080abf48989c216d1142cc8d7d99a7adf9bad6
Julian Mancera <julian.mancera@gameduell.de>
2015-12-08 16:31:27 +0100

    cleaned up the logger class and made it extern

--- logger/Logger.hx
+++ logger/Logger.hx
@@ -26,51 +26,7 @@
 
 package logger;
 
-#if js
-import js.html.Console;
-#end
-
-/**
-    @author jxav
- */
-@:final class Logger
+extern class Logger
 {
-
-///static
-#if flash
-    private static var tf: flash.text.TextField = null;
-#end
-
-    public static dynamic function print(v: Dynamic, ?pos: haxe.PosInfos = null) untyped
-    {
-#if flash
-        tf = flash.Boot.getTrace();
-        var s = flash.Boot.__string_rec(v,"");
-        tf.text +=s;
-#elseif neko
-        __dollar__print(v);
-#elseif php
-        php.Lib.print(v);
-#elseif js
-        var msg = js.Boot.__string_rec(v,"");
-        var d;
-        if( __js__("typeof")(document) != "undefined"
-        && (d = document.getElementById("haxe:trace")) != null ) {
-            msg = msg.split("\n").join("<br/>");
-            d.innerHTML += StringTools.htmlEscape(msg)+"<br/>";
-        }
-        else if (  __js__("typeof process") != "undefined"
-        && __js__("process").stdout != null
-        && __js__("process").stdout.write != null)
-            __js__("process").stdout.write(msg); // node
-        else if (  __js__("typeof console") != "undefined"
-        && __js__("console").log != null )
-            __js__("console").log(msg); // document-less js (which may include a line break)
-#elseif cs
-        cs.system.Console.Write(v);
-#elseif java
-        var str:String = v;
-        untyped __java__("java.lang.System.out.print(str)");
-#end
-    }
+    public static dynamic function print(v: Dynamic, ?pos: haxe.PosInfos = null) untyped;
 }
